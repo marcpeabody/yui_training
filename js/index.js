8d30ad2eace6e7588fb1755a58cc158d05b4346a
Marc Peabody <marcpeabody@yahoo.com>
2012-12-04 14:50:49 -0500

    listener

--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
-Y.use(['cssbutton', 'view'], function(Y){
+Y.use(['cssbutton', 'view', 'array-extras'], function(Y){
+    Y.ButtonView = Y.Base.create('buttonView', Y.View, [], {
+      template: "<button class='yui3-button'><div class='productDescription'>{description}</div><div class='productPrice'>({price})</div></button>",
+      render: function(){
+        var container = this.get('container'),
+            html      = Y.Lang.sub(this.template, this.get('model')),
+            node      = Y.Node.create(html),
+            model     = this.get('model');
+
+        node.setData(model);
+        container.setHTML(node);
+      }
+    });
+
     var data = [{sku: '12341234', description: 'Some thing', price: "15.00"},
                 {sku: '56785678', description: 'Other thing', price: "24.99"},
                 {sku: '78907890', description: 'Another thing', price: "35.99"}],
         productContainer = Y.one('#productContainer'),
-        productList = Y.Node.create('<ul></ul>'),
-        buttonTemplate = "<li><button class='yui3-button'><div class='productDescription'>{description}</div><div class='productPrice'>({price})</div></button></li>",
-        buttonListItem;
+        productList = Y.Node.create('<ul></ul>');
+
+    Y.Array.each(data, function(d){
+      var liNode  = Y.Node.create('<li></li>');
+      productList.append(liNode);
+      new Y.ButtonView({
+        container: liNode,
+        model: d
+      }).render();
+    });
+    Y.delegate('click', function(ev){
+      Y.log(ev.currentTarget.getData('sku'));
+    }, '#productContainer', 'ul li button');
 
     productContainer.append(productList);
-    Y.Array.each(data, function(item){
-        buttonListItem = Y.Node.create(Y.Lang.sub(buttonTemplate, item));
-        productList.append(buttonListItem);
-    });
 });
 
